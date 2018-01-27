65fba79bfa292534c10946a13cc3eb3e80922383
anderson c.r.e. (crea1g15) <crea1g15@soton.ac.uk>
2018-01-27 10:43:52 +0000

    fixed network manager

---
+++ Assets/Scripts/PlayerSpawnControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSpawnControl : NetworkManager {
+
+    public GameObject monsterPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
