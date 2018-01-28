3440fd904fb26549159752848af8299838aa6145
puddephatt r.g.s.m. (rgsp1g15) <rgsp1g15@soton.ac.uk>
2018-01-28 12:42:37 +0000

    sweeper

--- Assets/sweeper.cs
+++ Assets/sweeper.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class sweeper : MonoBehaviour {
     [SerializeField]
     Vector3 target1, target2;
-    Vector3 currenttarget;
-    bool istarget1 = true;
+    [SerializeField]
     float duration;
+    bool movetotarget1 = true;
     // Use this for initialization
     void Start () {
-        currenttarget = target1;
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.position = Vector3.Lerp(this.transform.position, currenttarget, duration);
-        if (this.transform.position == currenttarget)
+        this.transform.position = Vector3.Lerp(this.transform.position, (movetotarget1? target1:target2), duration);
+        if (Vector3.Distance(this.transform.position, target1) < 0.01f)
         {
-            currenttarget = istarget1 ? target2 : target1;
-            istarget1 = !istarget1;
+            movetotarget1 = false;
         }
-	}
+        if (Vector3.Distance(this.transform.position, target2) < 0.01f)
+        {
+            movetotarget1 = true;
+        }
+
+    }
 }
