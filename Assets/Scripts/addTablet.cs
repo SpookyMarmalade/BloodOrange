a11ae7558f02cfb59be3867eb5dd79a41d9b6c23
puddephatt r.g.s.m. (rgsp1g15) <rgsp1g15@soton.ac.uk>
2018-01-28 11:51:53 +0000

    surviver

---
+++ Assets/Scripts/addTablet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addTablet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.parent == null)
+            return;
+        PlayerActions playerActions = other.GetComponentInParent<PlayerActions>();
+        if (playerActions == null)
+            return;
+        Debug.Log("Dave");
+        playerActions.metadata += "tablet!";
+        Destroy(gameObject);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/sweeper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sweeper : MonoBehaviour {
+    [SerializeField]
+    Vector3 target1, target2;
+    Vector3 currenttarget;
+    bool istarget1 = true;
+    float duration;
+    // Use this for initialization
+    void Start () {
+        currenttarget = target1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Vector3.Lerp(this.transform.position, currenttarget, duration);
+        if (this.transform.position == currenttarget)
+        {
+            currenttarget = istarget1 ? target2 : target1;
+            istarget1 = !istarget1;
+        }
+	}
+}
